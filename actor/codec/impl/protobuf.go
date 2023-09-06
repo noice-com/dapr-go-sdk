84550c26673bfef74bbafe29eeaf349ed4dd54a6
Max Reuter <maxr@noice.com>
2023-09-06 15:48:21 +0300

    fix proto unmarshalling

--- actor/codec/impl/protobuf.go
+++ actor/codec/impl/protobuf.go
@@ -3,6 +3,7 @@ package impl
 import (
 	"errors"
 	"fmt"
+	"reflect"
 
 	"github.com/dapr/go-sdk/actor/codec"
 	"github.com/dapr/go-sdk/actor/codec/constant"
@@ -31,10 +32,30 @@ func (c *ProtobufCodec) Marshal(v interface{}) ([]byte, error) {
 }
 
 func (c *ProtobufCodec) Unmarshal(data []byte, v interface{}) error {
+	// Get the reflection value of the passed pointer.
+	vValue := reflect.ValueOf(v)
+
+	// Check if the passed pointer is a pointer.
+	if vValue.Kind() != reflect.Ptr {
+		return fmt.Errorf("ptr must be a pointer")
+	}
+
+	// Get the type of the underlying element that ptr points to.
+	targetType := vValue.Elem().Type()
+
+	newObjValue := reflect.New(targetType.Elem())
+
+	fmt.Println("NEW OBJ VALUE:", newObjValue.Type().String())
+
+	v = newObjValue.Interface()
+
 	m, ok := v.(proto.Message)
 	if !ok {
-		return fmt.Errorf("%w, got %T", ErrNotProtoMessage, v)
+		return fmt.Errorf("%w: got %T %#v", ErrNotProtoMessage, v, v)
 	}
+
+	// Assign the newBar to the pointer.
+	vValue.Elem().Set(newObjValue)
 
 	return proto.Unmarshal(data, m)
 }
